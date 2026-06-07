db9960c2d5c350cdf6c65f2ed2da8de8c423fd86
Howard J <enkru2000@hotmail.com>
2026-06-07 15:53:59 +1200

    vulnerabilities fixes

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,12 @@
 import 'vue';
 import type { Store } from 'vuex';
 
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
 declare module 'vue' {
   interface ComponentCustomProperties {
     $store: Store<any>;
